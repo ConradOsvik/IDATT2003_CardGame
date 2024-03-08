98989fdcf40afefd9469c47d4678322ec434a52c
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-03-08 01:27:42 +0100

    feat: init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module edu.ntnu.stud.cardgame {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.web;
+
+  requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+  requires net.synedra.validatorfx;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+  requires eu.hansolo.tilesfx;
+  requires com.almasb.fxgl.all;
+
+  opens edu.ntnu.stud.cardgame to javafx.fxml;
+  exports edu.ntnu.stud.cardgame;
+}
