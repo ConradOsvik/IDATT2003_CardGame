9d2c50bae460075098c9e662fd4fa0229ee1447b
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-03-13 23:37:07 +0100

    fix: fix indexing of cards in a deck

--- src/main/java/edu/ntnu/stud/cardgame/models/DeckOfCards.java
+++ src/main/java/edu/ntnu/stud/cardgame/models/DeckOfCards.java
@@ -4,21 +4,24 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class DeckOfCards {
+
   private final Random rand = new Random();
   private final char[] suits = {'S', 'H', 'D', 'C'};
   private final PlayingCard[] cards = new PlayingCard[52];
-  public DeckOfCards(){
-    for (char suit : suits) {
+
+  public DeckOfCards() {
+    for (int i = 0; i < suits.length; i++) {
+      char suit = suits[i];
       for (int face = 1; face <= 13; face++) {
-        cards[(suit - 1) * 13 + face - 1] = new PlayingCard(suit, face);
+        cards[i * 13 + face - 1] = new PlayingCard(suit, face);
       }
     }
   }
 
-  public HandOfCards dealHand(int n){
+  public HandOfCards dealHand(int n) {
     ArrayList<PlayingCard> hand = new ArrayList<>();
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
       int randIndex = rand.nextInt(52);
       hand.add(cards[randIndex]);
     }
