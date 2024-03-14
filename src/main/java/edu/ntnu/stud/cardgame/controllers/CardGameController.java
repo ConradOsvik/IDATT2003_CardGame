0f792462dcb68962c4b75e02f61615396bcf5136
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-03-14 01:43:13 +0100

    feat: add method for checking hand

--- src/main/java/edu/ntnu/stud/cardgame/controllers/CardGameController.java
+++ src/main/java/edu/ntnu/stud/cardgame/controllers/CardGameController.java
@@ -2,25 +2,36 @@ package edu.ntnu.stud.cardgame.controllers;
 
 import edu.ntnu.stud.cardgame.models.DeckOfCards;
 import edu.ntnu.stud.cardgame.models.HandOfCards;
+import edu.ntnu.stud.cardgame.models.PlayingCard;
 import edu.ntnu.stud.cardgame.views.WindowView;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import java.util.Arrays;
 import javafx.stage.Stage;
 
 public class CardGameController {
+
   private final WindowView view;
   private final DeckOfCards deck;
   private HandOfCards hand;
-  public CardGameController(Stage stage){
-      this.view = new WindowView(this);
-      stage.setTitle("CardGame");
-      stage.setScene(view);
 
-      this.deck = new DeckOfCards();
+  public CardGameController(Stage stage) {
+    this.view = new WindowView(this);
+    stage.setTitle("CardGame");
+    stage.setScene(view);
+
+    this.deck = new DeckOfCards();
   }
 
-  public void dealHand(){
+  public void dealHand() {
     this.hand = deck.dealHand(5);
     this.view.updateHand(hand);
+  }
+
+  public void checkHand() {
+    int sum = Arrays.stream(hand.getHand()).mapToInt(PlayingCard::getFace).sum();
+    PlayingCard[] hearts = Arrays.stream(hand.getHand()).filter(card -> card.getSuit() == 'H').toArray(PlayingCard[]::new);
+    boolean containsSpadesQueen = Arrays.stream(hand.getHand()).anyMatch(card -> card.getSuit() == 'S' && card.getFace() == 12);
+    boolean isFLush = hand.isFlush();
+
+    this.view.updateResult(sum, hearts, containsSpadesQueen, isFLush);
   }
 }
